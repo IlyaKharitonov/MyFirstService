b0cc3373274536f801d154e4bb0a20203174726a
ilya <Kharitonoilya@gmail.com>
2021-02-02 23:25:38 +0800

    v2

--- main.go
+++ main.go
@@ -1,36 +1,47 @@
 package main
 
 import (
-	_ "github.com/go-sql-driver/mysql"
+	"context"
 	"database/sql"
+	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
-	"flag"
+	"time"
 )
 
-func main(){
-	addr:=flag.String("addr",":8080","Адрес сервера")
-	addrDB:=flag.String("addrDB", "root:1643@(0.0.0.0:3306)/usersdb", "Адрес базы" )
+func main() {
+	addr := flag.String("addr", ":8080", "Server address")
+	addrDB := flag.String("addrDB", "root:1643@(0.0.0.0:3306)/usersdb", "Database address")
 
 	flag.Parse()
 
-
-	db,err := sql.Open("mysql", *addrDB)
-		if err != nil{
-			log.Fatal("Ошибка подключения к базе")
+	db, err := sql.Open("mysql", *addrDB)
+	if err != nil {
+		log.Fatal("Error connecting to the database when starting the service")
 	}
 	var storage Storager = Storage{
 		database: db,
 	}
 
-	srv := http.Server{Addr:*addr}
+	srv := http.Server{Addr: *addr}
 
- 	server := Server{
-		 httpsrv: srv,
-		 strg: storage,
+	server := Server{
+		httpsrv: srv,
+		strg:    storage,
 	}
 
- 	server.Run()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.httpsrv.Shutdown(ctx); err != nil {
+			log.Fatal("server stopped by signal")
+		}
+	}()
+
+	server.Run()
 
 }
-
